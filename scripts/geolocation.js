e7ed037c3eb8fe0807460039b4e450515f5305f9
sojida <sojidaniels@gmail.com>
2018-11-23 13:19:57 +0100

    fixed bugs on geoloaction and maps

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -6,22 +6,22 @@ locate_btn.addEventListener('click', (e) => {
     e.preventDefault()
     location_field.value = 'Locating...'
 
-    function success(position){
+    function success(position) {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-        location_field.value =`Latitude: ${latitude.toFixed(6)} Longitude: ${longitude.toFixed(6)}`
+        location_field.value = `${latitude.toFixed(6)} , ${longitude.toFixed(6)}`
 
         const latlon = latitude + "," + longitude;
 
 
-        const map_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=600x400&sensor=false&key=AIzaSyDb8u3PhTp_5VcFnRUhaA-fGLoOYqGyqQg";
+        const map_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=16&size=600x400&sensor=false&key=AIzaSyABDhT6kOJ31VVKeNgM4ByXbyhFdj-09TM";
 
-        document.getElementById("map").innerHTML = `<img src=${map_url}>`;
+        document.getElementById("map").innerHTML = `<img src=${map_url} alt='google map'>`;
 
     }
 
-    function onError(err){
-        switch(err.code) {
+    function onError(err) {
+        switch (err.code) {
             case err.PERMISSION_DENIED:
                 location_field.value = 'Denied request by User'
                 break;
@@ -35,15 +35,15 @@ locate_btn.addEventListener('click', (e) => {
                 location_field.value = 'An unknown error occurred'
                 break;
         }
-        
+
     }
 
-    if (!navigator.geolocation){
+    if (!navigator.geolocation) {
         location_field.value = 'Geolocation not available'
     } else {
         navigator.geolocation.getCurrentPosition(success, onError);
     }
 
-     
+
 })
 
