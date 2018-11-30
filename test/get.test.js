66bc43faf660ec5d45e7ff6a1518a9ef46febf7c
sojida <sojidaniels@gmail.com>
2018-11-30 02:28:44 +0100

    [finishes #162332625]test for GET api/v1/redflag/<id>

--- test/get.test.js
+++ test/get.test.js
@@ -6,7 +6,7 @@ import server from '../app';
 chai.use(chaiHttp);
 
 describe('Incidents', () => {
-  it('should respond with red-flag records', (done) => {
+  it('GET /api/v1/red-flags: should respond with red-flag records', (done) => {
     chai.request(server)
       .get('/api/v1/red-flags')
       .end((err, res) => {
@@ -21,7 +21,7 @@ describe('Incidents', () => {
       });
   });
 
-  it('should respond with intervention records', (done) => {
+  it('GET /api/v1/interventions: should respond with intervention records', (done) => {
     chai.request(server)
       .get('/api/v1/interventions')
       .end((err, res) => {
@@ -36,11 +36,64 @@ describe('Incidents', () => {
       });
   });
 
-  it('should respond with ierror on wrong input type', (done) => {
+  it('GET /api/v1/inter-redflag: should respond with error for wrong input type', (done) => {
     chai.request(server)
       .get('/api/v1/inter-redflag')
       .end((err, res) => {
         expect(res).to.have.status(400);
+        done();
+      });
+  });
+});
+
+
+describe('Specific incident', () => {
+  it('GET /api/v1/red-flags/<red-flag-id>: should respond with red-flag records', (done) => {
+    chai.request(server)
+      .get('/api/v1/red-flags/1')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+
+        const { data } = res.body;
+        const { type } = data[0];
+
+        expect(Array.isArray(data)).to.be.equal(true);
+        expect(data.length).to.be.equal(1);
+        expect(type).to.be.equal('red-flag');
+        done();
+      });
+  });
+
+  it('GET /api/v1/interventions: should respond with intervention records', (done) => {
+    chai.request(server)
+      .get('/api/v1/interventions/3')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+
+        const { data } = res.body;
+        const { type } = data[0];
+
+        expect(Array.isArray(data)).to.be.equal(true);
+        expect(data.length).to.be.equal(1);
+        expect(type).to.be.equal('intervention');
+        done();
+      });
+  });
+
+  it('GET /api/v1/intervention*/3: should respond with error on wrong input type', (done) => {
+    chai.request(server)
+      .get('/api/v1/inter-redflag')
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        done();
+      });
+  });
+
+  it('GET /api/v1/interventions/0: should respond with error', (done) => {
+    chai.request(server)
+      .get('/api/v1/inter-redflag')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
         done();
       });
   });
