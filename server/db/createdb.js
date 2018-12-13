c0de9486bf407e7ba5ba47dd544daa7bd7114d54
sojida <sojidaniels@gmail.com>
2018-12-13 10:35:05 +0100

    updated db files

--- server/db/createdb.js
+++ server/db/createdb.js
@@ -1,12 +1,11 @@
-import { createUsersTable, createIncidentTable } from './queries';
-import db from './user.db';
+import { createUsersTable, createIncidentTable } from './tables.queries';
+import db from './db';
 
 async function createAllTables() {
-    await db.query(createUsersTable());
-    await db.query(createIncidentTable());
+  await db.query(createUsersTable());
+  await db.query(createIncidentTable());
 }
 
 createAllTables()
-    .then(() => console.log('tables created'))
-    .catch((err) => console.log(err));
-
+  .then(() => console.log('tables created'))
+  .catch(err => console.log(err));
