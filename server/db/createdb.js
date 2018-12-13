05f9e5b8c0f436119a6853624816d9e1db6d49c1
sojida <sojidaniels@gmail.com>
2018-12-13 15:53:19 +0100

    seed admin

--- server/db/createdb.js
+++ server/db/createdb.js
@@ -1,9 +1,17 @@
 import { createUsersTable, createIncidentTable } from './tables.queries';
 import db from './db';
 
+const createAdmin = () => ({
+  text: 'INSERT INTO users(firstname, lastname, othernames, username, phoneNumber, email, isAdmin, registered, password) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
+  values: ['Adesoji', 'Daniel', 'Oluwagbogo', 'Superman', '09011111111', 'sojiman@gmail.com', 'True', 'NOW', 'admin1234'],
+});
+
+
+
 async function createAllTables() {
   await db.query(createUsersTable());
   await db.query(createIncidentTable());
+  await db.query(createAdmin())
 }
 
 createAllTables()
