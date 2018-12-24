1e3a933eaec94a496f9742b0e6443fe47d52f853
sojida <sojidaniels@gmail.com>
2018-12-24 23:06:27 +0100

    - added routes to get files from server - added express fileupload - added delete option to cors

--- server/app.js
+++ server/app.js
@@ -1,22 +1,12 @@
 import 'babel-polyfill';
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+import fileupload from 'express-fileupload';
 import routes from './routes/routes';
 import auth from './routes/auth.routes';
 
-
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({
-  type: '*/*',
-  extended: true,
-
-}));
-
-app.get('/', (req, res) => {
-  res.status(200).json({ welcome: 'IReporter Api' });
-});
 
 app.use(cors({
   credentials: true,
@@ -24,8 +14,30 @@ app.use(cors({
 }));
 
 
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(fileupload());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ welcome: 'IReporter Api' });
+});
+
+
 app.use('/api/v1/auth', auth);
 app.use('/api/v1', routes);
+
+app.get('/images/:name', (req, res) => {
+  fs.readFile(`server/uploads/images/${req.params.name}`, (err, data) => {
+    res.status(200).send(data);
+  });
+});
+
+app.get('/videos/:name', (req, res) => {
+  fs.readFile(`server/uploads/videos/${req.params.name}`, (err, data) => {
+    res.status(200).send(data);
+  });
+});
+
 
 app.use((req, res) => {
   res.status(404).json({ error: 'check documentation on routes' });
